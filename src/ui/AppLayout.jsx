928448cbd576016eba896c9ae67c0a8a19ae1e56
Vitalii Zorichev <zorichevitaliy@gmail.com>
2023-12-03 09:45:55 -0600

    fix import issue

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Header from "./Header";
-import SideBar from "./SIdeBar";
+import SideBar from "./SideBar";
 import { Outlet } from "react-router-dom";
 
 const StyledAppLayout = styled.div`
